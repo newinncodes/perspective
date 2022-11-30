4425329b8309eea17d96a2677d3e94c088343bdc
anewinn <64866105+anewinn@users.noreply.github.com>
2022-11-30 22:28:26 +0700

    Added third stage (beyond avg lifespan )

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // Global variables:
-let dif_future_days = 0;
 let dif_past_days = 0;
+let dif_toavg_days = 0;
+let dif_toend_days = 0;
 let dif_past_weeks = 0;
-let dif_future_weeks = 0;
+let dif_toavg_weeks = 0;
+let dif_toend_weeks = 0;
+let bolExplain = 0;
 
 // Run these functions on load:
 calcDays(120);
 createPerspective(1);
-animatePastFutureLabels();
+animateLabels();
 fit_to_height();
 window.onresize = fit_to_height;
+//pulse_heartrate(60);
 
 // Calling buttons to set lifespan age
 
@@ -38,37 +42,51 @@ function calcDays(intLifespan) {
   date_deathday.setFullYear(date_birthday.getFullYear() + intLifespan);
   const date_today = new Date();
 
+  const avglifeyears = 88
+  const date_avgdeathday = new Date(date_birthday.getTime());
+  date_avgdeathday.setFullYear(date_birthday.getFullYear() + avglifeyears);
+
   const dif_past_ms = date_today.getTime() - date_birthday.getTime();
   dif_past_days = Math.ceil(dif_past_ms / (1000 * 3600 * 24));
   dif_past_weeks = dif_past_days / 7;
 
-  const dif_future_ms = date_deathday.getTime() - date_today.getTime();
-  dif_future_days = Math.ceil(dif_future_ms / (1000 * 3600 * 24));
-  dif_future_weeks = dif_future_days / 7;
+  const dif_toavg_ms = date_avgdeathday.getTime() - date_today.getTime();
+  dif_toavg_days = Math.ceil(dif_toavg_ms / (1000 * 3600 * 24));
+  dif_toavg_weeks = dif_toavg_days / 7;
+
+  const dif_toend_ms = date_deathday.getTime() - date_avgdeathday.getTime();
+  dif_toend_days = Math.ceil(dif_toend_ms / (1000 * 3600 * 24));
+  dif_toend_weeks = dif_toend_days / 7;
+  
 }
 
 function createPerspective(intPeriod) {
   // Generate the text strings
   var str_past = "-";
-  var str_future = "-";
+  var str_toavg = "-";
+  var str_toend = "-";
   var int_pastPeriods = 1;
-  var int_futurePeriods = 1;
+  var int_toavgPeriods = 1;
+  var int_toendPeriods = 1;
   var lbl_Period = "";
 
   if (intPeriod == 0) {
     int_pastPeriods = dif_past_days;
-    int_futurePeriods = dif_future_days;
+    int_toavgPeriods = dif_toavg_days;
+    int_toendPeriods = dif_toend_days;
     lbl_Period = "days";
   } else if (intPeriod == 1) {
     int_pastPeriods = dif_past_days / 7;
-    int_futurePeriods = dif_future_days / 7;
+    int_toavgPeriods = dif_toavg_days / 7;
+    int_toendPeriods = dif_toend_days / 7;
     lbl_Period = "weeks";
   } else {
     int_pastPeriods = dif_past_days / 365;
-    int_futurePeriods = dif_future_days / 365;
+    int_toavgPeriods = dif_toavg_days / 365;
+    int_toendPeriods = dif_toend_days / 365;
     lbl_Period = "years";
   }
-  var str_future_repeated = str_future.repeat(dif_future_weeks);
+  //var str_future_repeated = str_toavg.repeat(dif_toavg_weeks);
 
   // Add in length of time to the strings:
   var str_past_label =
@@ -77,29 +95,50 @@ function createPerspective(intPeriod) {
     " " +
     lbl_Period +
     " ";
+  if (bolExplain==1){str_past_label="The time that have passed"}; // Explanation override
   var dif_past_weeks_half = (dif_past_weeks - str_past_label.length) / 2;
   var str_past_labeled =
     str_past.repeat(dif_past_weeks_half) +
     str_past_label +
     str_past.repeat(dif_past_weeks_half);
 
-  var str_future_label =
+  var str_toavg_label =
     " " +
-    int_futurePeriods.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, "$&,") +
+    int_toavgPeriods.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, "$&,") +
     " " +
     lbl_Period +
     " ";
-  var dif_future_weeks_half = (dif_future_weeks - str_future_label.length) / 2;
-  var str_future_labeled =
-    str_future.repeat(dif_future_weeks_half) +
-    str_future_label +
-    str_future.repeat(dif_future_weeks_half);
+  if (bolExplain==1){str_toavg_label="The time remaining until reaching an average life span"}; // Explanation override
+  var dif_toavg_weeks_half = (dif_toavg_weeks - str_toavg_label.length) / 2;
+  var str_toavg_labeled =
+    str_toavg.repeat(dif_toavg_weeks_half) +
+    str_toavg_label +
+    str_toavg.repeat(dif_toavg_weeks_half);
+
+  var str_toend_label =
+    " " +
+    int_toendPeriods.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, "$&,") +
+    " " +
+    lbl_Period +
+    " ";
+  if (bolExplain==1){str_toend_label="The time remaining beyond an average life span"}; // Explanation override
+  var dif_toend_weeks_half = (dif_toend_weeks - str_toend_label.length) / 2;
+  var str_toend_labeled =
+    str_toend.repeat(dif_toend_weeks_half) +
+    str_toend_label +
+    str_toend.repeat(dif_toend_weeks_half);
+  
+  //Add end to the labels
+  //str_toend_labeled = str_toend_labeled.split(0,-3)+"END";
 
   // Combine the strings together:
   var str_combined =
-    '<span style="background-color:#5B6C5D;">' + str_past_labeled + "</span>" + '<span style="background-color:#E06D06;">' + str_future_labeled + "</span>";
-  var str_past_combined = '<span style="background-color:#5B6C5D;">' + str_past_labeled + "</span>"
-  var str_future_combined = '<span style="background-color:#E06D06;">' + str_future_labeled + "</span>";
+    '<span style="background-color:#5B6C5D;">' + str_past_labeled + "</span>" 
+    + '<span style="background-color:#E06D06;">' + str_toavg_labeled + "</span>"
+    + '<span style="background-color:#611C35;">' + str_toend_labeled + "</span>";
+  //var str_past_combined = '<span style="background-color:#5B6C5D;">' + str_past_labeled + "</span>"
+  //var str_toavg_combined = '<span style="background-color:#E06D06;">' + str_toavg_labeled + "</span>";
+  //TODO fade the end of the string
 
   // Send to HTML:
   document.getElementById("output_generated_combined").innerHTML = str_combined;
@@ -110,8 +149,8 @@ function createPerspective(intPeriod) {
 // ------------------------------------
 
 // Animate the label with a timed loop
-function animatePastFutureLabels() {
-  const refreshRate = 200000 / 60;
+function animateLabels() {
+  const refreshRate = 300000 / 60;
   let lbl_typenum = 1;
 
   window.setInterval(() => {
@@ -127,8 +166,8 @@ function animatePastFutureLabels() {
 // Pulse the main perspective map like a heartbeat
 async function pulse_heartbeat() {
   colourpulse();
-  await new Promise(resolve => setTimeout(resolve, 300));
-  colourpulse();
+  //await new Promise(resolve => setTimeout(resolve, 300));
+  //colourpulse();
 
 }
 
@@ -165,15 +204,30 @@ async function colourpulse() {
 }
 
 // Unused test function - to get something on the site pulsing like a heartrate
-/*
+
 function pulse_heartrate(intBPM) {
   const milliseconds_between_beats = 1 / (intBPM / 60) * 1000;
   window.setInterval(() => {
     pulse_heartbeat();
   }, milliseconds_between_beats);
-  pulse_heartbeat;
+  //pulse_heartbeat;
 }
-*/
+
+// FUNCTION:  To toggle explanations or counters
+async function toggle_labels(){
+  let butToggle = document.getElementById("but_toggle_labels");
+  const ToggleStates = ["explain", "time"];
+
+  if (butToggle.innerHTML==ToggleStates[0]){
+    butToggle.innerHTML=ToggleStates[1];
+    bolExplain=1;
+    createPerspective(0);
+  } else {
+    butToggle.innerHTML=ToggleStates[0];
+    bolExplain=0;
+    createPerspective(0);
+  }
+}
 
 
 function fit_to_height(){
@@ -187,6 +241,8 @@ function fit_to_height(){
 
   // get perspective string:
   let output = document.getElementById("output_generated_combined");
+  let firstline = document.getElementById("firstline");
+  let butToggle = document.getElementById("but_toggle_labels");
   let strCombined = output.innerHTML;
 
   // calculate dimensions for new font size:
@@ -201,5 +257,6 @@ function fit_to_height(){
   const fontScaler = 1.18; // Adjustment by this scaler as height of font greater than width and we want to fill the space
    
   output.style.fontSize=(fontWidthPX * fontScaler) +"px";  // Adjust by 1.2 as fontheight is larger than fontwidth in monospace
-
+  firstline.style.fontSize=(fontWidthPX * fontScaler) +"px";  // Adjust by 1.2 as fontheight is larger than fontwidth in monospace
+  butToggle.style.fontSize=(fontWidthPX * fontScaler) +"px";  // Adjust by 1.2 as fontheight is larger than fontwidth in monospace
 }
